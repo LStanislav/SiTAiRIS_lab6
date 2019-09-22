3e4bdbfd208014af01dd14e1e0781e07cdeeaa12
Stas <lstanislav1924@mail.ru>
2019-09-22 12:59:29 +0300

    First commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.print("Введите N: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int sum = 0;
+        if (n >= 1) {
+            for (int i = 1; i <= n; i++) {
+                sum += i;
+            }
+        }
+        else {
+            for (int i=1; i>=n; i--){
+                sum+=i;
+            }
+        }
+        System.out.println("Сумма от 1 до N: " + sum);
+        // write your code here
     }
 }
